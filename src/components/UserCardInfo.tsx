a0684a8dc391a563bec8762043e9ac688396f930
ptzt <ignaciotomasrodriguezmillan@gmail.com>
2023-05-31 11:20:16 -0300

    responsive in progress

--- src/components/UserCardInfo.tsx
+++ src/components/UserCardInfo.tsx
@@ -1,11 +1,54 @@
 import React from "react";
-import GithubOctocat from "./icons/githubOctocat";
+import GithubOctocat from "@/components/icons/GithubOctocat";
 
 const UserCardInfo = () => {
   return (
-    <article className="rounded-xl bg-blue-900 p-2 text-white">
-      <div className=" grid h-24 w-24 place-content-center rounded-full bg-gray-200 p-1 ">
+    <article className="grid-areas grid rounded-xl bg-blue-900 p-2 text-white">
+      <div className="section-logo grid h-20 w-20 place-content-center rounded-full bg-gray-200 p-1 ">
         <GithubOctocat className="relative top-2 h-full w-full" />
+      </div>
+      <div className="section-title">
+        <h2>The Octocat</h2>
+        <p>@octocat</p>
+      </div>
+      <p className="section-data">Joined 25 Jan 2011</p>
+      <p className="section-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
+        laboriosam vel ad praesentium repellendus id blanditiis autem nihil
+        voluptates dolores eligendi possimus, earum delectus corrupti iure.
+        Provident, dicta maiores? Esse?
+      </p>
+      <div className="section-number flex justify-around rounded-lg bg-blue-950">
+        <article>
+          <p>Repos</p>
+          <p>8</p>
+        </article>
+        <article>
+          <p>Followers</p>
+          <p>3000</p>
+        </article>
+        <article>
+          <p>Following</p>
+          <p>9</p>
+        </article>
+      </div>
+      <div className="section-social md:grid md:grid-cols-2">
+        <article>
+          <i>Icono</i>
+          <p>San fracisco</p>
+        </article>
+        <article>
+          <i>Icono</i>
+          <p>San fracisco</p>
+        </article>
+        <article>
+          <i>Icono</i>
+          <p>Twitter</p>
+        </article>
+        <article>
+          <i>Icono</i>
+          <p>@github</p>
+        </article>
       </div>
     </article>
   );
