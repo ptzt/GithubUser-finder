75a6af4269d81b519af59a973db59646cac488cc
ptzt <ignaciotomasrodriguezmillan@gmail.com>
2023-05-31 15:54:45 -0300

    add error when no found user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,27 @@ import React, { useState } from "react";
 import { User } from "@/interfaces/user";
 
 const Home = () => {
-  const [user, setUser] = useState<User | null>();
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<string | null>();
 
   const getUser = async (username: string) => {
     const res = await fetch(`https://api.github.com/users/${username}`);
+    if (!res.ok) {
+      setUser(null);
+      setError("User not found");
+      return;
+    }
     setUser(await res.json());
+    setError(null);
   };
 
   return (
     <>
       <FormSearchUser getUser={getUser} />
       {user && <UserCardInfo user={user} />}
+      {error && (
+        <div className="rounded-lg bg-red-500 p-4 text-white">{error}</div>
+      )}
     </>
   );
 };
